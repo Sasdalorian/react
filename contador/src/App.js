7ebe3c2b8d7a35b4f4a2e1433c3b9385a6b17cd4
Sasdalorian <esteban.nicolas.sd@gmail.com>
2023-05-30 17:20:40 -0400

    terminando Contador de Clicks

--- contador/src/App.js
+++ contador/src/App.js
@@ -1,14 +1,41 @@
 import './App.css';
+import logo from './img/logoSas.png';
+import { Boton } from './componentes/Boton';
+import { Contador } from './componentes/Contador';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0); 
+  
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  };
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+
   return (
     <div className="App">
-      <div className="freecodecamp-logo-contenedor">
+      <div className="logo-contenedor">
         <img 
-          className='freecodecamp-logo'
-          src="a"
+          className='logo'
+          src={logo}
           alt="No se ha podido cargar logo"
         />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics}/>
+          <Boton 
+            texto="Clic"
+            esBotonDeClic={true}
+            manejarClic={manejarClic}
+          />
+          <Boton 
+            texto="Reiniciar"
+            esBotonDeClic={false}
+            manejarClic={reiniciarContador}
+          />
       </div>
     </div>
   );
