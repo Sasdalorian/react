fadae09652400ce13fa89488cec78acc0506669e
Sasdalorian <esteban.nicolas.sd@gmail.com>
2023-05-30 10:42:13 -0400

    Comenzando Proyecto Contador de Clicks

---
+++ contador/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img 
+          className='freecodecamp-logo'
+          src="a"
+          alt="No se ha podido cargar logo"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
